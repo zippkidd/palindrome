251799cee2ac5430a87c2fd034e3f7eb37105f0f
Thomas Tylek <35775622+zippkidd@users.noreply.github.com>
2018-04-11 11:35:04 -0400

    Added palindrome.h

---
+++ palindrome.h
@@ -0,0 +1,14 @@
+//
+//  Palindrome.h
+//  Lab8.2
+//
+//  Created by Thomas Tylek on 3/2/15.
+//  Copyright (c) 2015 Thomas Tylek. All rights reserved.
+//
+
+#ifndef __Lab8_2__Palindrome__
+#define __Lab8_2__Palindrome__
+
+#include <stdio.h>
+
+#endif /* defined(__Lab8_2__Palindrome__) */
